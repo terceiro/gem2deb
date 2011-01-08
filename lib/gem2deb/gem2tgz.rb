f5caa7ac1b90b9a64926d09f53addd2f4f7bf5d7
Lucas Nussbaum <lucas@lucas-nussbaum.net>
2011-01-08 16:23:02 +0100

    continue integration in gem2deb

--- lib/gem2deb/gem2tgz.rb
+++ lib/gem2deb/gem2tgz.rb
@@ -14,68 +14,65 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'fileutils'
+require 'tmpdir'
 
-class Gem2Tgz
+require 'gem2deb'
+include Gem2Deb
 
-  VERSION = '0.1.0'
+module Gem2Deb
 
-  class CommandFailed < Exception
-  end
+  class Gem2Tgz
 
-  def self.convert!(gem, tarball)
-    self.new(gem, tarball).convert!
-  end
-
-  attr_reader :gem, :gem_full_path
-  attr_reader :tarball, :tarball_full_path
-  attr_reader :target_dir
-
-  def initialize(gem, tarball)
-    @gem                = gem
-    @gem_full_path      = File.expand_path(gem)
-    @tarball            = tarball
-    @tarball_full_path  = File.expand_path(tarball)
-    @target_dir         = tarball_full_path.gsub('.tar.gz', '')
-  end
-
-  def convert!
-    create_target_dir
-    extract_gem_contents
-    create_resulting_tarball
-    cleanup
-  end
-
-  protected
-
-  def create_target_dir
-    FileUtils.mkdir_p(target_dir)
-  end
-
-  def extract_gem_contents
-    Dir.chdir(target_dir) do
-      run "tar xfm #{gem_full_path}"
-      run 'tar xzfm data.tar.gz'
-      FileUtils.rm_f('data.tar.gz')
-      run "zcat metadata.gz > metadata.yml"
-      FileUtils.rm_f('metadata.gz')
+    def self.convert!(gem, tarball)
+      self.new(gem, tarball).convert!
     end
-  end
 
-  def create_resulting_tarball
-    Dir.chdir(File.dirname(tarball_full_path)) do
-      run "tar czf #{File.basename(tarball)} #{File.basename(target_dir)}"
+    attr_reader :gem, :gem_full_path
+    attr_reader :tarball, :tarball_full_path
+    attr_reader :target_dir
+
+    def initialize(gem, tarball)
+      @gem                = gem
+      @gem_full_path      = File.expand_path(gem)
+      @tarball            = tarball
+      @tarball_full_path  = File.expand_path(tarball)
+      @target_dirname     = File::basename(tarball).gsub('.tar.gz', '')
     end
-  end
 
-  def cleanup
-    FileUtils.rm_rf(target_dir)
-  end
-
-  def run(cmd)
-    system(cmd)
-    if $? && ($? >> 8) > 0
-      raise Gem2Tgz::CommandFailed, "[#{cmd} failed!]"
+    def convert!
+      create_target_dir
+      extract_gem_contents
+      create_resulting_tarball
+      cleanup
     end
-  end
 
+    protected
+
+    def create_target_dir
+      @tmp_dir = Dir::mktmpdir('gem2tgz')
+      @target_dir = @tmp_dir + '/' + @target_dirname
+      Dir::mkdir(@target_dir)
+    end
+
+    def extract_gem_contents
+      Dir.chdir(@target_dir) do
+        run "tar xfm #{gem_full_path}"
+        run 'tar xzfm data.tar.gz'
+        FileUtils.rm_f('data.tar.gz')
+        run "zcat metadata.gz > metadata.yml"
+        FileUtils.rm_f('metadata.gz')
+      end
+    end
+
+    def create_resulting_tarball
+      Dir.chdir(@tmp_dir) do
+        run "tar czf #{@tarball_full_path} #{@target_dirname}"
+      end
+    end
+
+    def cleanup
+      FileUtils.rm_rf(@tmp_dir)
+    end
+
+  end
 end
