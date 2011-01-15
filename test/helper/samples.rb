c9f9c2f9c4b28b765460ac6ba52988f235bccd3d
Antonio Terceiro <terceiro@softwarelivre.org>
2011-01-15 15:00:28 -0300

    Easier name for tmp directories

--- test/helper/samples.rb
+++ test/helper/samples.rb
@@ -1,7 +1,7 @@
 class Gem2DebTestCase
   module Samples
     SAMPLE_DIR            = File.join(File.dirname(__FILE__), '..', 'sample')
-    TMP_DIR               = Dir.mktmpdir
+    TMP_DIR               = Dir.mktmpdir(['gem2deb.', '.tmp'])
 
     SIMPLE_GEM_DIRNAME    = 'simplegem-0.0.1'
     SIMPLE_GEM            = File.join(SAMPLE_DIR, "simplegem/pkg/#{SIMPLE_GEM_DIRNAME}.gem")
