terceiro/gem2deb

commit f70afe11498c442bd1ee858c72644591fedafc74

Improve the maintainer search from ENV
Signed-off-by: Laurent Arnoud <laurent@spkdev.net>

lib/gem2deb/dh-make-ruby.rb
@@ -24,6 +24,7 @@ module Gem2Deb
 
   class DhMakeRuby
 
+    EMAIL_REGEXP = /^(.*)\s+<(.*)>$/
     attr_reader :gem_name
 
     def initialize(tarball)
@@ -137,6 +138,25 @@ http://pkg-ruby-extras.alioth.debian.org/cgi-bin/gemwatch/#{@gem_name} .*/#{@gem
       end
     end
 
+    ##
+    # Try to find the maintainer from ENV
+    # logic translated from perl in package « devscripts: /usr/bin/dch »
+    #
+    def maintainer
+      debenv = {}
+      if ENV['DEBEMAIL'] && ENV['DEBEMAIL'] =~ EMAIL_REGEXP
+        debenv['DEBFULLNAME'] = $1 if ENV['DEBFULLNAME'].nil?
+        debenv['DEBEMAIL'] = $2
+      end
+      if ENV['DEBEMAIL'].nil? || ENV['DEBFULLNAME']
+        if ENV['EMAIL'] && ENV['EMAIL'] =~ EMAIL_REGEXP
+          debenv['DEBFULLNAME'] = $1 if ENV['DEBFULLNAME'].nil?
+          debenv['DEBEMAIL'] = $2
+        end
+      end
+      debenv
+    end
+
     def create_control
       f = File::new("#{@gem_name}-#{@gem_version}/debian/control", 'w')
       f.puts <<-EOF
@@ -144,7 +164,7 @@ Source: #{@gem_name}
 Section: ruby
 Priority: optional
 Maintainer: Debian Ruby Extras Maintainers <pkg-ruby-extras-maintainers@lists.alioth.debian.org>
-Uploaders: #{ENV['DEBFULLNAME']} <#{ENV['DEBEMAIL']}>
+Uploaders: #{maintainer['DEBFULLNAME']} <#{maintainer['DEBEMAIL']}>
 DM-Upload-Allowed: yes
 Build-Depends: debhelper (>= 7.0.50~), gem2deb (>= #{Gem2Deb::VERSION})
 Standards-Version: 3.9.1

commit 35a5590e6c3a3bcf3649acc235fa1757fbf59ffd

Fix introduced bug for debemail and debfullname
introduced by f70afe11498c442bd1ee858c72644591fedafc74
user with DEBFULLNAME and DEBEMAIL env will result to nothing on control file.
Add some defaults and fix the EMAIL case
(missing .nil? on ENV['DEBFULLNAME'])

Signed-off-by: Laurent Arnoud <laurent@spkdev.net>

lib/gem2deb/dh-make-ruby.rb
@@ -144,11 +144,19 @@ http://pkg-ruby-extras.alioth.debian.org/cgi-bin/gemwatch/#{@gem_name} .*/#{@gem
     #
     def maintainer
       debenv = {}
+      # defaults
+      debenv['DEBFULLNAME'] = ENV['DEBFULLNAME']
+      debenv['DEBEMAIL'] = ENV['DEBEMAIL'] || ENV['EMAIL']
+
+      # DEBEMAIL is like "Full Name <email@host>"
+      # extract DEBFULLNAME from it
       if ENV['DEBEMAIL'] && ENV['DEBEMAIL'] =~ EMAIL_REGEXP
         debenv['DEBFULLNAME'] = $1 if ENV['DEBFULLNAME'].nil?
         debenv['DEBEMAIL'] = $2
       end
-      if ENV['DEBEMAIL'].nil? || ENV['DEBFULLNAME']
+      # dont have DEBEMAIL nor DEBFULLNAME from ENV
+      # try with EMAIL
+      if ENV['DEBEMAIL'].nil? || ENV['DEBFULLNAME'].nil?
         if ENV['EMAIL'] && ENV['EMAIL'] =~ EMAIL_REGEXP
           debenv['DEBFULLNAME'] = $1 if ENV['DEBFULLNAME'].nil?
           debenv['DEBEMAIL'] = $2