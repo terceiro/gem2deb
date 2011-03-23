ac0fa6c67e7b0776b2866871903bf23564b37561
Lucas Nussbaum <lucas@lucas-nussbaum.net>
2011-03-23 16:48:35 +0100

    Rewrite depends after dh_shlibdeps to create ORed deps.

--- debhelper7/sequence/ruby.pm
+++ debhelper7/sequence/ruby.pm
@@ -5,8 +5,6 @@ use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
 
-#insert_after("dh_perl", "dh_python2");
-#remove_command("dh_pycentral");
-#remove_command("dh_pysupport");
+insert_after("dh_shlibdeps", "dh_ruby_fixdepends");
 
 1
