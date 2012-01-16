5892534ac59df1304642e4aa6f7be942edd6bc78
Antonio Terceiro <terceiro@debian.org>
2012-01-16 17:45:52 -0200

    load a development version in the environment

---
+++ tools/shell.bash
@@ -0,0 +1,6 @@
+#!/bin/sh
+gem2deb_root=$(readlink -f $BASH_SOURCE | xargs dirname | xargs dirname)
+export PATH="${gem2deb_root}/bin:${PATH}"
+export PERL5LIB="${gem2deb_root}/debhelper7"
+export RUBYLIB="${gem2deb_root}/lib"
+PS1="\033[31;40;01m** gem2deb debug **\033[m $PS1"
