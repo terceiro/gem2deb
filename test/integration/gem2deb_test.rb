3faaa2bc4d976d6609afc31db49d81abb61880ad
Antonio Terceiro <terceiro@debian.org>
2014-03-30 13:03:12 -0300

    fix one test to work without Ruby 1.9 support
    the test was assuming an non-multiarch binary extension installation
    path.

--- test/integration/gem2deb_test.rb
+++ test/integration/gem2deb_test.rb
@@ -77,7 +77,7 @@ class Gem2DebTest < Gem2DebTestCase
 
   self.build_tree('test/sample/simpleextension_dh_auto_install_destdir') do |dir|
     should 'honor DH_RUBY_USE_DH_AUTO_INSTALL_DESTDIR when building extensions' do
-      assert Dir.glob("#{dir}/debian/tmp/usr/lib/ruby/vendor_ruby/**/*.so").size > 0, 'no .so files found in debian/tmp/'
+      assert Dir.glob("#{dir}/debian/tmp/**/*.so").size > 0, 'no .so files found in debian/tmp/'
     end
   end
 
