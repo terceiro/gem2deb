c45b5be1936ebbad7bc4c904e171c123caadcba0
Christian Hofstaedtler <zeha@debian.org>
2015-04-08 18:43:11 +0200

    Remove rdoc stamp files at install time
    The rdoc stamp files are hindering the reproducible build effort.
    Instead of depriving rdoc itself of it's minimal rebuild functionality,
    we remove the stamp files at package build time. We assume that the
    stamp files only end up inside /usr/share/doc/<package>, and that they're
    installed no later than dh_installdocs.

--- debhelper7/Debian/Debhelper/Sequence/ruby.pm
+++ debhelper7/Debian/Debhelper/Sequence/ruby.pm
@@ -7,6 +7,8 @@ use Debian::Debhelper::Dh_Lib;
 
 insert_after("dh_shlibdeps", "dh_ruby_fixdepends");
 
+insert_after("dh_installdocs", "dh_ruby_fixdocs");
+
 add_command_options("dh_compress", "-X.rb");
 
 1
---
+++ test/integration/dh_ruby_fixdocs_test.rb
@@ -0,0 +1,46 @@
+require_relative '../test_helper'
+
+class DhRubyFixDocsTest < Gem2DebTestCase
+
+  should 'remove created.rid from doc folder' do
+    docfiles = prepare('foo') do
+      docfiles = Dir.glob("debian/foo/**/*")
+      assert docfiles.any? { |filename| filename =~ /created.rid/ }, "#{docfiles.inspect} expected to include created.rid after prepare"
+
+      run_command('dh_ruby_fixdocs')
+
+      Dir.glob("debian/foo/**/*")
+    end
+
+    assert docfiles.none? { |filename| filename =~ /created.rid/ }, "#{docfiles.inspect} expected to not include created.rid"
+  end
+
+  def prepare(package)
+    pkgdir = File.join(tmpdir, package)
+    FileUtils.mkdir(pkgdir)
+    Dir.chdir(pkgdir) do
+      FileUtils.mkdir 'debian'
+
+      File.open('debian/control', 'w') do |control|
+        control.puts("Source: #{package}")
+        control.puts('Maintainer: The Maintainer <maintainer@example.com>')
+        control.puts('XS-Ruby-Versions: all')
+        control.puts
+        control.puts("Package: #{package}")
+        control.puts('Architecture: any')
+        control.puts('Depends: ${shlibs:Depends}, ruby | ruby-interpreter')
+        control.puts('Description: example package')
+        control.puts(' Just for testing')
+      end
+
+      FileUtils.mkdir_p 'debian/foo/usr/share/doc/foo/rdoc'
+      File.open('debian/foo/usr/share/doc/foo/rdoc/created.rid', 'w') do |f|
+        f.puts("Stub rdoc timestamp file")
+      end
+
+      yield
+    end
+  end
+
+end
+
