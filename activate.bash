fd6bf023a3dc5ca08238ad1cec2e35186cda4cab
Antonio Terceiro <terceiro@debian.org>
2015-06-12 10:51:20 -0300

    Add bash script to use gem2deb from the source tree

---
+++ activate.bash
@@ -0,0 +1,6 @@
+# source this script from your shell to override the gem2deb installed
+# system-wide with the one from the source tree.
+
+gem2deb_root=$(dirname "$BASH_SOURCE")
+export PATH="${gem2deb_root}/bin:${PATH}"
+export RUBYLIB="${gem2deb_root}/lib"
