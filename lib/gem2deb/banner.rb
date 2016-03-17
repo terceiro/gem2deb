677c69d7e933da3b962796ce397aeb29be1a8ce6
Antonio Terceiro <terceiro@debian.org>
2016-03-17 11:36:12 -0300

    Gem2Deb::Banner: flush stdout immediately

--- lib/gem2deb/banner.rb
+++ lib/gem2deb/banner.rb
@@ -1,11 +1,14 @@
 module Gem2Deb
   module Banner
     def self.print(msg)
-      puts
-      puts '┌' + '─' * 78 + '┐'
-      puts '│ %-77s│' % msg
-      puts '└' + '─' * 78 + '┘'
-      puts
+      $stdout.instance_eval do
+        puts
+        puts '┌' + '─' * 78 + '┐'
+        puts '│ %-77s│' % msg
+        puts '└' + '─' * 78 + '┘'
+        puts
+        flush
+      end
     end
   end
 end
