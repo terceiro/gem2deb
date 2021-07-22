635f122f010b42b04b90338ba61b64d0b869af7e
Antonio Terceiro <terceiro@debian.org>
2021-07-22 08:23:11 -0300

    debhelper: reformat Perl code with perltidy
    Gbp-Dch: ignore

--- debhelper/Debian/Debhelper/Buildsystem/ruby.pm
+++ debhelper/Debian/Debhelper/Buildsystem/ruby.pm
@@ -10,44 +10,44 @@ use strict;
 use base 'Debian::Debhelper::Buildsystem';
 
 sub DESCRIPTION {
-	"Ruby (Gem2Deb)"
+    "Ruby (Gem2Deb)";
 }
 
 sub check_auto_buildable {
-	my $this=shift;
-	return (-e $this->get_sourcepath("metadata.yml")) ? 1 : 0;
+    my $this = shift;
+    return ( -e $this->get_sourcepath("metadata.yml") ) ? 1 : 0;
 }
 
 sub new {
-	my $class=shift;
-	my $this=$class->SUPER::new(@_);
-	$this->enforce_in_source_building();
-	return $this;
+    my $class = shift;
+    my $this  = $class->SUPER::new(@_);
+    $this->enforce_in_source_building();
+    return $this;
 }
 
 sub configure {
-	my $this=shift;
-	$this->doit_in_sourcedir("dh_ruby", "--configure", @_);
+    my $this = shift;
+    $this->doit_in_sourcedir( "dh_ruby", "--configure", @_ );
 }
 
 sub build {
-	my $this=shift;
-	$this->doit_in_sourcedir("dh_ruby", "--build", @_);
+    my $this = shift;
+    $this->doit_in_sourcedir( "dh_ruby", "--build", @_ );
 }
 
 sub test {
-	my $this=shift;
-	$this->doit_in_sourcedir("dh_ruby", "--test", @_);
+    my $this = shift;
+    $this->doit_in_sourcedir( "dh_ruby", "--test", @_ );
 }
 
 sub install {
-	my $this=shift;
-	$this->doit_in_sourcedir("dh_ruby", "--install", @_);
+    my $this = shift;
+    $this->doit_in_sourcedir( "dh_ruby", "--install", @_ );
 }
 
 sub clean {
-	my $this=shift;
-	$this->doit_in_sourcedir("dh_ruby", "--clean", @_);
+    my $this = shift;
+    $this->doit_in_sourcedir( "dh_ruby", "--clean", @_ );
 }
 
 1
--- debhelper/Debian/Debhelper/Sequence/ruby.pm
+++ debhelper/Debian/Debhelper/Sequence/ruby.pm
@@ -5,10 +5,10 @@ use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
 
-insert_after("dh_shlibdeps", "dh_ruby_fixdepends");
+insert_after( "dh_shlibdeps", "dh_ruby_fixdepends" );
 
-insert_after("dh_installdocs", "dh_ruby_fixdocs");
+insert_after( "dh_installdocs", "dh_ruby_fixdocs" );
 
-add_command_options("dh_compress", "-X.rb");
+add_command_options( "dh_compress", "-X.rb" );
 
 1
