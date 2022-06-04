aed83b49faf36879f7cb86c0f3779ca74f4397c2
Antonio Terceiro <terceiro@debian.org>
2022-06-04 20:25:13 -0300

    source_test: replace mocking with temporary directory
    This will help later when Gem2Deb::Source is made read other files under
    the debian/ directory.

--- test/unit/source_test.rb
+++ test/unit/source_test.rb
@@ -3,17 +3,30 @@ require 'gem2deb/source'
 
 class SourceTest < Gem2DebTestCase
 
-  context "selecting package layout" do
+  def debian_control
+    @debian_control ||= []
+  end
 
-    setup do
-      @source = Gem2Deb::Source.new
+  def t_tmpdir
+    @t_tmpdir ||= File.join(tmpdir, method_name).tap do |dir|
+      FileUtils.mkdir_p(File.join(dir, "debian"))
     end
+  end
+
+  def source
+    @source ||= Dir.chdir(t_tmpdir) do
+      File.write("debian/control", debian_control.join("\n"))
+      Gem2Deb::Source.new
+    end
+  end
+
+  context "selecting package layout" do
 
     should 'default to single-binary' do
       debian_control << 'Package: ruby-foo'
       debian_control << 'Package: ruby-bar'
       ruby_foo = { :binary_package => 'ruby-foo', :root => '.' }
-      assert_equal [ruby_foo], @source.send(:packages)
+      assert_equal [ruby_foo], source.send(:packages)
     end
 
     should 'ignore packages without X-DhRuby-Root when one of them has it' do
@@ -22,7 +35,7 @@ class SourceTest < Gem2DebTestCase
       debian_control << 'X-DhRuby-Root: bar'
 
       ruby_bar = { :binary_package => 'ruby-bar', :root => 'bar' }
-      assert_equal [ruby_bar], @source.send(:packages)
+      assert_equal [ruby_bar], source.send(:packages)
     end
 
     should 'detect multibinary' do
@@ -32,7 +45,7 @@ class SourceTest < Gem2DebTestCase
       debian_control << 'X-DhRuby-Root: bar'
       ruby_foo = { :binary_package => 'ruby-foo', :root => 'foo' }
       ruby_bar = { :binary_package => 'ruby-bar', :root => 'bar' }
-      assert_equal [ruby_foo, ruby_bar], @source.send(:packages)
+      assert_equal [ruby_foo, ruby_bar], source.send(:packages)
     end
 
   end
